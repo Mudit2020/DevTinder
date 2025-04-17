b2973d064e6e729d9eae5a9db107e3f6e06781ff
muditrayvoria <Mudit.Rayvoria@anyonehome.com>
2025-04-18 02:47:15 +0530

    Routes

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
-app.use("/",(req, res) => {
-    res.send("Hi, Server is build succesfully");
+
+
+// http://localhost:8000/queries?userId=101&name=mudit
+app.get("/queries",(req, res) => {
+    console.log(req.query);
+    res.send("Hello");
 })
 
-app.listen(PORT,(req, res) => {
+//http://localhost:8000/dynamicroute/101/mudit
+app.get("/dynamicroute/:userID/:name",(req, res) => {
+    console.log(req.params);
+    console.log(req.params.userID);
+    console.log(req.params.name);
+    res.send("Hello, It is basically used when we pass data in url dynamically. \n so, we can access that data using above way.");
+})
+
+app.listen(PORT,()=>{
     console.log(`Server is running on port number ${PORT}`);
 })
