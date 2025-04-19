a53780cbd977aa870c323286f8d9699f8df16eb6
muditrayvoria <Mudit.Rayvoria@anyonehome.com>
2025-04-19 20:51:54 +0530

    New Method In Routing

--- src/app.js
+++ src/app.js
@@ -2,22 +2,95 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
-// http://localhost:8000/queries?userId=101&name=mudit
-app.get("/queries",(req, res) => {
-    console.log(req.query.userId);
-    console.log(req.query.name);
-    res.send("Hello");
-})
 
-// http://localhost:8000/dynamicroute/101/mudit
-app.get("/dynamicroute/:userID/:name",(req, res) => {
-    console.log(req.params.userID);
-    console.log(req.params.name);
-    console.log(req.params);
-    res.send("Hello, It is basically used when we pass data in url dynamically. \n so, we can access that data using above way.");
-})
+// //Next method() . It is basically used to transfer the control form one route handle to other Route handler. 
+
+// //Below one is working fine.
+// app.get("/", (req, res, next) => {
+//     console.log("1st Response");
+//     next(); // It is used for passing the control.
+// });
 
 
+// app.get("/", (req, res) => {
+//     res.send("2nd response");
+// });
+
+
+
+
+
+
+
+// // Error :- Cannot set headers after they are sent to the client
+// app.get("/", (req, res, next) => {
+//     res.send("1st Response");
+//     next(); 
+// });
+
+// app.get("/", (req, res) => {
+//     res.send("2nd response");
+// });
+
+
+
+// // Error :- Cannot set headers after they are sent to the client
+// app.get("/", (req, res, next) => {
+//     next();
+//     res.send("1st Response"); 
+// });
+
+// app.get("/", (req, res) => {
+//     res.send("2nd response");
+// });
+
+
+
+// // Error :- Cannot set headers after they are sent to the client
+// app.get("/", (req, res, next) => {
+//     res.send("1st Response"); 
+//     next();
+// });
+
+// app.get("/", (req, res, next) => {
+//     res.send("2nd response");
+//     next();
+// });
+
+
+
+// // The below code goes into the hand state.because there is no response sendby the client.
+// app.get("/", (req, res, next) => {
+//     console.log("1st Response"); 
+//     next();
+// });
+
+// app.get("/", (req, res) => {
+//     // res.send("2nd Response");
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+app.get("/", (req, res, next) => {
+    console.log("1st Response"); 
+    next();
+});
+
+app.get("/", (req, res) => {
+    res.send("2nd Response");
+});
+
+
+// // Note:- server ek hi baar response deta hai ek request ke liye. Isliye, ek se jaada res.send work nahi kar rhe hai.
 
 
 
