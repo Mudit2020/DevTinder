fa6382df89b7b9a1fcbe16c57fead27e0d89ecce
muditrayvoria <Mudit.Rayvoria@anyonehome.com>
2025-05-22 01:18:06 +0530

    DB_Creation

--- src/app.js
+++ src/app.js
@@ -2,23 +2,33 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
+const connectDB = require("./config/database");
+const User = require("./models/user");
 
-app.get("/getUserData", (req, res) => {
-    throw new Error ("cnwekfnrej");
-    res.send("User Data Sent")
-})
+app.get("/",((req, res)=> {
+    res.send("You are on home page");
+}))
 
-
-//below code is basically used. when upar agar kisi bhi route main error handle agr nahi hoti hai to wo neeche waaley 
-// route se handle ho jayegi.
-// Remember order of code matter alot.
-
-app.use("/", (err, req, res, next) => {
-    if(err) {
-        res.status(500).send("error occurred");
+app.post("/signup", async (req, res) => {
+    const data = {
+        firstName : "Mudit",
+        latsName : "Rayvoria",
+        email : "muditrayvoria@gmail.com",
+        password : "mudit@123",
+        age : 24,
+        gender : "Male"
     }
+    const user = new User(data);
+    await user.save();
+    res.send("user added succesfully");
 })
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port number ${PORT}`);
+connectDB()
+.then(()=>{
+    console.log("Database successfully Connected");
+    app.listen(PORT,()=>{
+        console.log(`Server is running on port number ${PORT}`);
+    })
+}).catch((err) => {
+    console.log("Not connected successfully.", err);
 })
---
+++ src/config/database.js
@@ -0,0 +1,10 @@
+
+
+const mongoose = require("mongoose");
+
+const connectDB = async ()=>{
+    await mongoose.connect("mongodb://localhost:27017/practice");
+}
+
+module.exports = connectDB;
+
