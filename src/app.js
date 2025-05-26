ccf4f9c2d6dbc07f4e486e01df7afad949dddd15
muditrayvoria <Mudit.Rayvoria@anyonehome.com>
2025-05-27 00:52:16 +0530

    get_method

--- src/app.js
+++ src/app.js
@@ -7,6 +7,54 @@ const User = require("./models/user");
 
 app.use(express.json());
 
+
+//There are many functions which is used for fetching a data from the DB.
+// .find({});
+// .findOne({});
+// .findById({});
+ 
+// Below code is when data is present in DB. And, we just need to find out then we can simply get that data
+// by just passing an existing key in the Database. It will return an array of data.
+// .find() :- It will gives you all data from the db with same email in the array form.
+//data.length :- This function is used for finding the length of the records which is present in the DB.
+
+
+//Get users by email.
+app.get("/feed", async (req, res) => {
+    try {
+        const data = await User.find({email : req.body.email});
+        if(data.length === 0) {
+            res.status(400).send("User not found");
+        }
+        else {
+            res.send(data);
+        }    
+    }
+    catch(err) {
+        res.status(400).send("Data not found");
+    }
+})
+
+
+//Feed API - Get /feed - get all the users from the database.
+// .find({})  :- If we are using like this then we will get all the document from the collection.
+
+app.get("/feedAllData", async (req, res) => {
+    try {
+        const data = await User.find({});
+        res.send(data);
+    }
+    catch(err) {
+        res.send("Collection is empty. There is not document.");
+    }
+})
+
+// .find() :- this method gives us the array of data
+// .findOne() :- It only gives one data. Not the whole array of data.
+
+
+
+
 app.post("/signup", async (req, res) => {
     const user = new User(req.body);
     try {
@@ -17,6 +65,10 @@ app.post("/signup", async (req, res) => {
         res.status(400).send("Error saving the user : ", err.message);
     }
 })
+
+app.use("/",(req, res) => {
+    res.send("There is some issue in our code");
+});
 
 connectDB()
 .then(()=>{
